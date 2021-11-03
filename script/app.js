89b34aa9ff75f60603e2df2366d3bd74b0c71e60
Md. Saadman Galib Rabbi <galib2829@gmail.com>
2021-11-03 09:47:18 +0600

    Updating files

--- script/app.js
+++ script/app.js
@@ -4,7 +4,16 @@
 var navBtn = document.querySelector('.nav-btn')
 var navListContainer = document.querySelector('.nav-items')
 
+var a = 0
+
 navBtn.addEventListener("click", function(){
+    if (a <=0){
+        navListContainer.style.right = '100vw'
+        a += 1 
+    }
+    // else{
+    //     pass
+    // }
     // console.log('Hi')
     if (navListContainer.style.right == '100vw'){
         navListContainer.style.right = '-30px';
